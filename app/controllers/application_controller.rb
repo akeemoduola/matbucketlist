2c4a932d534720fe1649e15c0d3ffbf01ffd9cf9
andela-aoduola <akeem.oduola@andela.com>
2016-01-26 11:17:09 +0100

    Token Based Authentication
    Why: So users' can only access resources they have authority to access
    
    This change addresses the need by:
    *Creating a User model to store user details
    *Generating a AuthController that manage users' authentication token
    *Adding the method authenticate_request! to ApplicationController to
    authenticate each request and set current_user if valid token is
    supplied
    
    [Finishes #111680702]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,56 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request!
+  attr_reader :current_user
+
+  # When an error occurs, respond with the proper private method below
+  rescue_from AuthenticationTimeoutError, with: :authentication_timeout
+  rescue_from NotAuthenticatedError, with: :user_not_authenticated
+
+  protected
+
+  # This method gets the current user based on the user_id included
+  # in the Authorization header (json web token).
+  def authenticate_request!
+    fail NotAuthenticatedError unless user_id_included_in_auth_token?
+    @current_user = User.find(decoded_auth_token[0]["user_id"])
+    activate(@current_user)
+  rescue JWT::ExpiredSignature
+    raise AuthenticationTimeoutError
+  rescue JWT::VerificationError, JWT::DecodeError
+    raise NotAuthenticatedError
+  end
+
+  private
+
+  # Authentication Related Helper Methods
+  # ------------------------------------------------------------
+  def user_id_included_in_auth_token?
+    http_auth_token && decoded_auth_token && decoded_auth_token[0]["user_id"]
+  end
+
+  # Decode the authorization header token and return the payload
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_token)
+  end
+
+  def http_auth_token
+    @http_auth_token ||= if request.headers['Authorization'].present?
+                           request.headers['Authorization'].split(' ').last
+                         end
+  end
+
+  # Check if user is logged in#
+  def activate(user)
+    unless user && user.active?
+      render json: { Error: "You must login first" }, status: 401
+    end
+  end
+
+  # Helper Methods for responding to errors#
+  def authentication_timeout
+    render json: { errors: ['Token has expired. Re-login for a new token.'] }, status: 419
+  end
+  def user_not_authenticated
+    render json: { errors: ["You are not authorized to perform this action."] }, status: :unauthorized
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  post 'auth/login'
+
+  get 'auth/logout'
+
+  resources :users, :only => ["create"]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
